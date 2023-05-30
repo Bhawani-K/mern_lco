344e74b1f571cd78015240c17d3d4cbf62798c88
Bhawani_K <kundala.bhawani@outlook.com>
2023-05-30 08:14:27 +0530

    feature_added dbConnection

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
-
-
 const PORT = 7000;
 
-app.listen(PORT, (req,res)=>{
-    console.log(`Server started at port ${PORT}`);
+//COMMENT : MongoDb Connection
+mongoose.connect('mongodb://localhost:27017/tshirt', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true
+})
+.then(()=> console.log('DB CONNECTED ---'))
+.catch((err)=> console.log(err))
+//NOTE : Connecting Functions :  
+
+app.listen(PORT, (req, res) => {
+    console.log(` Server started at port ${PORT}`);
 })
